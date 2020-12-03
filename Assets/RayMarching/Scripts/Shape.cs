308595cad6606eaeaf8cf115c0b6159f54f70320
WSWhitehouse <williamswhitehouse@gmail.com>
2020-12-03 02:02:04 +0000

    Worked on rotations

--- Assets/RayMarching/Scripts/Shape.cs
+++ Assets/RayMarching/Scripts/Shape.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using WSWhitehouse.RayMarching.Enums;
+using WSWhitehouse.RayMarching.Util;
 
 namespace WSWhitehouse.RayMarching
 {
@@ -16,7 +17,19 @@ namespace WSWhitehouse.RayMarching
         public Color Colour => colour;
 
         public Vector3 Position => transform.position;
-        public Vector3 Rotation => transform.rotation.eulerAngles;
+
+        public Vector3 Rotation
+        {
+            get
+            {
+                Vector3 eulerAngles = transform.rotation.eulerAngles;
+                float x = Util.Rotation.DegreesToRightAngles(eulerAngles.x);
+                float y = eulerAngles.y;
+                float z = eulerAngles.z;
+                return Util.Rotation.EulerAnglesToRadians(new Vector3(x, y, z));
+            }
+        }
+
 
         public Vector3 Scale
         {
